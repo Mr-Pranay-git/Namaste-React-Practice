0d237dcfa04e612b951a702cdce6145fcd79e51c
Mr pranay-git <pranaymanusmare2@gmail.com>
2025-07-06 23:24:04 +0530

    few changes after initial commit

--- episode1/App.js
+++ episode1/App.js
@@ -1,8 +1,31 @@
+/*
+  <div id="parent">
+    <div id="child">
+    <h1></h1>
+    </div>
+    </div>
+*/
+
+
+const parent = React.createElement(
+    "div",
+    {id:"parent"},  
+    React.createElement(
+        "div",
+        {id:"child"}, 
+        React.createElement("h1",
+            {id: "sub-child"},
+            "I'm an h1 tag")
+        )
+);
+
+
 const heading = React.createElement(
     "h1",
     {id: "heading"}, 
     "Hello World from React!"
-)
+);
+console.log(heading); // this will return  object
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
 
-const root = ReactDom.createRoot(document.getElementById("root"));
-root.render(heading)
